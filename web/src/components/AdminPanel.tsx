6cc39b980f5070499e571e44ec8428b82a357923
Abdukhami Doff <hammagayetadijamoasi@gmail.com>
2026-08-01 15:09:53 +0500

    fix: import useEffect in AdminPanel.tsx to prevent runtime crash

--- web/src/components/AdminPanel.tsx
+++ web/src/components/AdminPanel.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+
 import { useApp } from '../context/AppContext';
 import { Recipe, Tale } from '../types';
 import { compressImage, uploadImageToSupabase, uploadImageWithStatus } from '../utils/imageCompressor';
